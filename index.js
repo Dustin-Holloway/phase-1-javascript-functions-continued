756a5ef60cf1d9511b94594ff7d7c84a57539df4
Dustin <dustinhollowaypdx@gmail.com>
2023-03-20 10:52:25 -0700

    functions continued commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(string = "roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+saturdayFun("ride a bike")
+
+function mondayWork(string = "go to the office") {
+    return `This Monday, I will ${string}.`
+
+}
+
+function wrapAdjective(parameter = "*") {
+       
+        return function(param = "special"){
+            return `You are ${parameter}${param}${parameter}!`
+        }
+        
+    
+            
+        }
+    
+
